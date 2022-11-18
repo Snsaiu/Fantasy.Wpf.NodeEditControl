465b7f0669f55c8675f79d081aca1471000dfeb2
zhulinshu <zhulinshu@qq.com>
2022-11-18 12:39:31 +0800

    创建贝塞尔对象

---
+++ Fantasy.Wpf.NodeEditControl/Controls/Lines/BezierLine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Fantasy.Wpf.NodeEditControl.Controls.Lines
+{
+    public class BezierLine : LineBase
+    {
+        public override void UpdateColor(SolidColorBrush color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateEndPoint(Point point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateLineWidth(int width)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateStartPoint(Point point)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
